fef76b861f46c8559f06eae2315c18685f35012f
Jakobi Haskell <jakobi_haskell@brown.edu>
2024-02-24 19:08:24 -0500

    adds more instructions in readme

--- backend/serverless/functions/src/auth/createUser.ts
+++ backend/serverless/functions/src/auth/createUser.ts
@@ -4,4 +4,5 @@ import * as logger from "firebase-functions/logger";
 export const createUser = user().onCreate(user => {
   logger.info("Creating a new user", { structuredData: true });
   // TODO SANA: create a new user in user table with userId, email, name
+  // Look at Firebase docs for structure of user object
 });
--- backend/serverless/functions/src/url/updateSettings.ts
+++ backend/serverless/functions/src/url/updateSettings.ts
@@ -4,6 +4,7 @@ import { onRequest } from "firebase-functions/v2/https";
 export const updateSettings = onRequest((request, response) => {
   logger.info("Updating user settings", { structuredData: true });
   // TODO CHRISTIAN: update device settings in device table with new breakingNewsAlerts, weeklySummaryAlerts
-    
+    // Assume request body is { breakingNewsAlerts: true, weeklySummaryAlerts: false } or subset of these fields
+    // and if it is not, return an error status code
   response.send("Settings updated!");
 });
