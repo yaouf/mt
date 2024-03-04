ed03eecf7d2346ec842ba61717b1e80cd5b994ea
Kiera Walsh <77820964+k-walsh@users.noreply.github.com>
2024-03-04 13:50:08 -0500

    search sprint 1
    Co-authored-by: RachelChae <rachel_chae@brown.edu>

---
+++ frontend/src/components/Item.tsx
@@ -0,0 +1,30 @@
+// item.tsx
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+interface ItemProps {
+  item: {
+    title: string;
+    content: string;
+    metadata?: object; // Optional Params
+    ssts_id?: string; // Optional Params
+  };
+}
+
+const Item: React.FC<ItemProps> = ({ item }) => {
+  return (
+    <View style={styles.itemContainer}>
+      <Text style={styles.itemTitle}>{item.title}</Text>
+      <Text>{item.content}</Text>
+      {/* {item.metadata && <Text>Metadata: {JSON.stringify(item.metadata)}</Text>}
+      {item.ssts_id && <Text>SSTS ID: {item.ssts_id}</Text>} */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  itemContainer: {},
+  itemTitle: { color: "brown" },
+});
+
+export default Item;
--- frontend/src/pages/SearchScreen.tsx
+++ frontend/src/pages/SearchScreen.tsx
@@ -1,4 +1,7 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput, Pressable, FlatList } from "react-native";
+import { StyleSheet } from "react-native";
+import React, { useState } from "react";
+import Item from "../components/Item";
 
 /**
  * Page for search (separate page just for now)
@@ -9,12 +12,77 @@ import { View, Text } from "react-native";
  *
  * @returns Search screen
  */
+
 function SearchScreen() {
+  const [text, onChangeText] = useState("");
+  const [searchResults, setSearchResults] = useState("");
+  const [articles, setArticles] = useState([]);
+
+  const fetchSearchResults = async () => {
+    try {
+      // Fetch a response using the Search.JSON URL Link (Currently restricted to basic searches)
+      const response = await fetch(
+        `https://www.browndailyherald.com/search.json?a=1&o=date&s=${text}`
+      );
+      const jsonString = await response.text();
+      setSearchResults(jsonString); // Sets search results, format this later
+
+      const resultObject = JSON.parse(jsonString); // Parse string into an object
+      setSearchResults(resultObject);
+
+      const articleList = resultObject.items; // List of articles
+      setArticles(articleList);
+      console.log(articleList);
+    } catch (error) {
+      console.error("Error" + error);
+    }
+  };
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Search!</Text>
+      <Text style={styles.titleText}>Search</Text>
+      <TextInput
+        onChangeText={onChangeText}
+        placeholder="Input Query Here!"
+        value={text}
+        style={{
+          paddingLeft: 50,
+          height: 40,
+          width: 200,
+          borderColor: "gray",
+          borderWidth: 3,
+          marginBottom: 10,
+          borderRadius: 20,
+          alignContent: "center",
+        }}
+      />
+      <Pressable style={styles.button} onPress={fetchSearchResults}>
+        <Text style={{ color: "white", paddingLeft: 10, paddingTop: 7 }}>
+          {" "}
+          Submit{" "}
+        </Text>
+      </Pressable>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => <Item item={item} />}
+        style={{ width: "70%" }}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  titleText: {
+    fontSize: 40,
+    fontWeight: "bold",
+    paddingBottom: 20,
+  },
+  button: {
+    backgroundColor: "brown",
+    height: 30,
+    width: 70,
+    borderRadius: 10,
+  },
+});
 
 export default SearchScreen;
