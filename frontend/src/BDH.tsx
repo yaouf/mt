508c6c1efe0f449cb53f33d6fb16fcb1062450e6
kwong60 <123611046+kwong60@users.noreply.github.com>
2024-03-04 12:57:16 -0500

    Article Component + Transition (#7)
    * intercept+redirect to article
    
    * redirect to article component!
    
    * removed tabs header
    
    * swipe transition!
    
    ---------
    
    Co-authored-by: Kiera Walsh <77820964+k-walsh@users.noreply.github.com>

--- frontend/src/BDH.tsx
+++ frontend/src/BDH.tsx
@@ -6,27 +6,51 @@ import HomeScreen from "./pages/HomeScreen";
 import SectionsScreen from "./pages/SectionsScreen";
 import SearchScreen from "./pages/SearchScreen";
 import SettingsScreen from "./pages/SettingsScreen";
+import ArticleScreen from "./components/Article";
+import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from "./pages/LoginScreen";
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+type YourParamListType = {
+ Home: undefined;
+ Article: { articleUrl: string };
+};
+const HomeStack = createStackNavigator<YourParamListType>();
+
+const horizontalTransition = ({ current, layouts }: any) => {
+  const translateX = current.progress.interpolate({
+    inputRange: [0, 1],
+    outputRange: [layouts.screen.width, 0],
+  });
+
+
+  return { cardStyle: { transform: [{ translateX }] } };
+};
+
+const HomeStackScreen = () => (
+ <HomeStack.Navigator>
+   <HomeStack.Screen name="Home" component={HomeScreen} options={{ headerShown: false , }} />
+   <HomeStack.Screen name="Article" component={ArticleScreen} options={{cardStyleInterpolator: horizontalTransition, }} />
+ </HomeStack.Navigator>
+);
 
 /**
  * @returns Main app component
  */
 export default function BDH() {
-  useEffect(() => {
-    registerForPushNotificationsAsync();
-  }, []);
+ useEffect(() => {
+   registerForPushNotificationsAsync();
+ }, []);
 
-  return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Sections" component={SectionsScreen} />
-        <Tab.Screen name="Search" component={SearchScreen} />
-        <Tab.Screen name="Login" component={LoginScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+ return (
+   <NavigationContainer>
+     <Tab.Navigator>
+       <Tab.Screen name="Home" component={HomeStackScreen} options={{ headerShown: false }}/>
+       <Tab.Screen name="Sections" component={SectionsScreen} />
+       <Tab.Screen name="Search" component={SearchScreen} />
+       <Tab.Screen name="Settings" component={SettingsScreen} />
+     </Tab.Navigator>
+   </NavigationContainer>
+ );
 }
--- frontend/src/pages/HomeScreen.tsx
+++ frontend/src/pages/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import { WebView } from "react-native-webview";
+import { WebView , WebViewNavigation } from "react-native-webview";
 import { StyleSheet } from "react-native";
 import { useRef } from "react";
 import { View, Button } from "react-native";
@@ -14,45 +14,64 @@ type HomeProps = {
  * @param param navigation param from BDH parent component
  * @returns Home Screen
  */
+
 function Home({ navigation }: HomeProps) {
   const webviewRef = useRef<WebView>(null);
-
+ 
+  // Define the function to handle navigation state change
+  const handleNavigationStateChange = (navState: any) => {
+    if (navState.canGoBack) {
+      navigation.setOptions({
+        headerLeft: () => (
+          <Button
+            onPress={() => webviewRef.current?.goBack()}
+            title="Back"
+            color="#000"
+          />
+        ),
+      });
+    } else {
+      navigation.setOptions({
+        headerLeft: () => null,
+      });
+    }
+  };
+ 
+  // Define the function to determine whether to load the request or not
+  const shouldStartLoadWithRequest = (event: WebViewNavigation) => {
+    const url = event.url || '';
+ 
+    // Check if the URL contains the specific string
+    if (url.includes('/article/')) {
+      // Redirect to Article component
+      navigation.navigate('Article', { articleUrl: url });
+      // Do not load the URL
+      return false;
+    }
+    // Continue loading for other URLs
+    return true;
+  };
+ 
   return (
     <View style={styles.container}>
       <WebView
         ref={webviewRef}
-        source={{ uri: "https://www.browndailyherald.com/" }}
+        source={{ uri: 'https://www.browndailyherald.com/' }}
         style={styles.webview}
-        onNavigationStateChange={(navState) => {
-          // The WebView is not at the top of its history stack
-          if (navState.canGoBack) {
-            navigation.setOptions({
-              headerLeft: () => (
-                <Button
-                  onPress={() => webviewRef.current?.goBack()}
-                  title="Back"
-                  color="#000"
-                />
-              ),
-            });
-          } else {
-            navigation.setOptions({
-              headerLeft: () => null,
-            });
-          }
-        }}
+        onNavigationStateChange={handleNavigationStateChange}
+        onShouldStartLoadWithRequest={shouldStartLoadWithRequest}
       />
     </View>
   );
-}
-
-export default Home;
-
-const styles = StyleSheet.create({
+ }
+ 
+ export default Home;
+ 
+ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
   webview: {
     flex: 1,
   },
-});
+ }); 
