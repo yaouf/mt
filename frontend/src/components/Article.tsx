4da7090cd57cfa1d707220dd2f7501edb0262cd2
kwong60 <123611046+kwong60@users.noreply.github.com>
2024-03-04 12:57:16 -0500

    Article Component + Transition (#7)
    * intercept+redirect to article
    
    * redirect to article component!
    
    * removed tabs header
    
    * swipe transition!
    
    ---------
    
    Co-authored-by: Kiera Walsh <77820964+k-walsh@users.noreply.github.com>

--- frontend/src/components/Article.tsx
+++ frontend/src/components/Article.tsx
@@ -1,19 +1,43 @@
-import { View, Text } from "react-native";
+import React from 'react';
+import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
+import { StackScreenProps } from '@react-navigation/stack';
+
+type YourParamListType = {
+ Home: undefined; // No additional parameters for the Home screen
+ Article: { articleUrl: string }; // Parameter for the Article screen
+};
+
+type ArticleProps = StackScreenProps<YourParamListType, 'Article'>;
 
 /**
- * When click on article, will redirect to this native article component
- * instead of opening in webview
- * 
- * Will integrate into webview redirect and add smooth transitions
- * 
- * @returns native Article component
- */
-function Article() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Article native component!</Text>
-    </View>
-  );
+* When click on article, will redirect to this native article component
+* instead of opening in webview
+*
+* Will integrate into webview redirect and add smooth transitions
+*
+* @returns native Article component
+*/
+function Article({ route }: ArticleProps) {
+ const { articleUrl } = route.params;
+
+ return (
+   <View style={styles.container}>
+     <WebView
+     source={{ uri: articleUrl }}
+     style={styles.webview}
+     />
+   </View>
+ );
 }
+
+const styles = {
+ container: {
+   flex: 1,
+ },
+ webview: {
+   flex: 1,
+ },
+};
 
 export default Article;
