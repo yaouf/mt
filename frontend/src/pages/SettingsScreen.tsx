bae34422d68cd61d300b906b2736056a39f49d74
asing122 <118394893+asing122@users.noreply.github.com>
2024-03-04 13:15:17 -0500

    annika/feature/login (#6)
    * structure of settings page
    
    * added login page, shared state between login and settings
    
    ---------
    
    Co-authored-by: Kiera Walsh <77820964+k-walsh@users.noreply.github.com>

--- frontend/src/pages/SettingsScreen.tsx
+++ frontend/src/pages/SettingsScreen.tsx
@@ -1,20 +1,92 @@
-import { View, Text } from "react-native";
+import { Dispatch, SetStateAction, useState } from "react";
+import { View, Text, Switch, Pressable, StyleSheet } from "react-native";
+
+type LoginProps = {
+  loggedIn: boolean;
+  setLoggedIn: Dispatch<SetStateAction<boolean>>;
+  username: string;
+  setUsername: Dispatch<SetStateAction<string>>;
+};
 
 /**
- * Page for settings 
+ * Page for settings
  *   - show login button if not already logged in
  *   - if logged in, show name/email
  *   - option to allow push notifications (send id to backend)
  *   - toggle to choose different types of notifications
  *   - option to delete account
  *   - contact us / feedback ?
- * 
+ *
  * @returns Settings screen
  */
-function Settings() {
+function Settings({
+  loggedIn,
+  setLoggedIn,
+  username,
+  setUsername,
+}: LoginProps) {
+  // const [name, setName] = useState<string>("");
+  const [pushNotifs, setPushNotifs] = useState<boolean>(false);
+  // const [loggedIn, setLoggedIn] = useState<boolean>(false);
+
+  const styles = StyleSheet.create({
+    button: {
+      backgroundColor: "#808080",
+      padding: 10,
+      borderRadius: 5,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    buttonText: {
+      color: "#fff",
+      fontSize: 16,
+    },
+  });
+
+  function handleLogin() {
+    setLoggedIn(true);
+    setUsername("X"); //??
+    console.log("Logged in.");
+  }
+
+  function handleDelete() {
+    console.log("Account deleted.");
+  }
+
+  function handlePushNotifs() {
+    setPushNotifs((previousState) => !previousState);
+  }
+
+  function handleContact() {
+    console.log("Contact!");
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Settings!</Text>
+      {loggedIn ? (
+        <>
+          <Text>{username}</Text>
+          <Pressable onPress={handleDelete} style={styles.button}>
+            <Text style={styles.buttonText}>Delete account</Text>
+          </Pressable>
+          <Text>Push Notifications</Text>
+          <Switch value={pushNotifs} onValueChange={handlePushNotifs} />
+          <Pressable onPress={handleContact} style={styles.button}>
+            <Text style={styles.buttonText}>Contact us</Text>
+          </Pressable>
+        </>
+      ) : (
+        <>
+          <Pressable onPress={handleLogin} style={styles.button}>
+            <Text style={styles.buttonText}>Login</Text>
+          </Pressable>
+          <Text>Push Notifications</Text>
+          <Switch value={pushNotifs} onValueChange={handlePushNotifs} />
+          <Pressable onPress={handleContact}>
+            <Text>Contact us</Text>
+          </Pressable>
+        </>
+      )}
     </View>
   );
 }
