008bed05e8e82d4758a3c6471841375cd23fc5f7
Jakobi Haskell <jakobi_haskell@brown.edu>
2024-03-06 20:07:44 -0500

    Feat/createuser (#12)
    * adds succesful create user and firebase auth emulator for testing
    
    Co-authored-by: Sana Saab sana_saab@brown.edu"
    
    * Revert "adds succesful create user and firebase auth emulator for testing"
    
    This reverts commit 49f8c280a30d32af43e7108d276db59bd843c0ef.
    
    * Revert "Revert "adds succesful create user and firebase auth emulator for testing""
    
    This reverts commit 12daf7d17d373e236be20d564f751e6994fec5ee.

--- backend/serverless/functions/src/auth/createUser.ts
+++ backend/serverless/functions/src/auth/createUser.ts
@@ -1,8 +1,25 @@
 import { user } from "firebase-functions/v1/auth";
 import * as logger from "firebase-functions/logger";
+import db from "../../../../db/data/db-config";
 
-export const createUser = user().onCreate(user => {
+export const createUser = user().onCreate(async user => {
   logger.info("Creating a new user", { structuredData: true });
   // TODO SANA: create a new user in user table with userId, email, name
   // Look at Firebase docs for structure of user object
+
+   // Insert new user into SQL table
+   try {
+     await db("users").insert({
+       userId: user.uid,
+       email: user.email,
+       name: user.displayName,
+     });
+     logger.info("User added to database successfully", {
+       structuredData: true,
+     });
+   } catch (error) {
+     logger.error("Error adding user to database:", error, {
+       structuredData: true,
+     });
+   }
 });
