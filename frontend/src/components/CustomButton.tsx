a538f9950c65aa6cb62a97751efbc577549be1fa
rachelchae <98571205+rachelchae@users.noreply.github.com>
2024-03-11 21:06:10 -0400

    feat: custom button props and user is able to pass in styles and text and onPress func (#14)

--- frontend/src/components/CustomButton.tsx
+++ frontend/src/components/CustomButton.tsx
@@ -1,7 +1,42 @@
-import { Text } from "react-native";
+import { Text, ViewStyle, TouchableOpacity, StyleSheet } from "react-native";
 
-function CustomButton() {
-  return <Text>make a reusable button here</Text>;
+interface CustomButtonProps {
+  text: string;
+  onPress: () => void;
+  buttonColor?: string; // Optional
+  textColor?: string; // Optional
+  style?: ViewStyle; // Allows for custom styles when we pass in our own styles
 }
+
+// Custom Button Component
+export const CustomButton: React.FC<CustomButtonProps> = ({
+  text,
+  onPress,
+  style = {},
+}) => (
+  <TouchableOpacity
+    onPress={onPress}
+    activeOpacity={0.7}
+    style={[styles.button, style]} // This array merges default style with custom style
+  >
+    <Text style={[styles.buttonText, style]}>{text}</Text>
+  </TouchableOpacity>
+);
+
+// Default Styling for Custom Button - when user does not pass in their own styles
+const styles = StyleSheet.create({
+  button: {
+    height: 30,
+    width: 70,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "brown",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+});
 
 export default CustomButton;
