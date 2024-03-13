8ab4780c28d9482c6b67bc3f5e0e8759b1349a01
Sana Saab <sana_saab@brown.edu>
2024-03-13 14:25:35 -0400

    Feat createdevice (#17)
    * please test (consider using try catch?)
    
    * added a return id
    
    * wraps everything in try catch and returns deviceId
    
    * added some comments and finished
    
    * added some comments and finished
    
    * done
    
    ---------
    
    Co-authored-by: Jakobi Haskell <jakobi_haskell@brown.edu>

--- backend/serverless/functions/src/url/createDevice.ts
+++ backend/serverless/functions/src/url/createDevice.ts
@@ -4,20 +4,36 @@ import db from "../../../../db/data/db-config";
 
 export const createDevice = onRequest(async (request, response) => {
   logger.info("Creating a new device", { structuredData: true });
-  // TODO CHRISTIAN: create a new device in device table with deviceId, deviceType, breakingNewsAlerts, weeklySummaryAlerts, expoPushToken? (optional)
+  // TODO SANA: create a new device in device table with deviceId, deviceType, breakingNewsAlerts, weeklySummaryAlerts, expoPushToken? (optional)
   // Assume info above is in request body as json. If any required fields are missing, return an error status code
-  const insertedRows = await db("devices").insert({
-    deviceType: "Android",
-    breakingNewsAlerts: true,
-    weeklySummaryAlerts: false,
-    expoPushToken: "ExpoPushToken[124]",
-  });
 
-  console.log(insertedRows);
+  try {
+    // Extract device information from the request body
+    // The "|| {}" portion at the end ensures that if there are any issues with request.body line 12 won't throw an error
+    const {deviceType, breakingNewsAlerts, weeklySummaryAlerts, expoPushToken} = request.body || {};
+    if (!deviceType || typeof breakingNewsAlerts !== 'boolean' || typeof weeklySummaryAlerts !== 'boolean') {
+      response.status(400).send("Missing required fields in request body");
+      return;
+    }
+    
+    // Insert the device into the devices table, and return the id of the inserted row
+    const insertedRows = await db("devices").insert({
+      deviceType: deviceType, 
+      breakingNewsAlerts: breakingNewsAlerts,
+      weeklySummaryAlerts: weeklySummaryAlerts,
+      expoPushToken: expoPushToken, // Might be inserted as null (if not provided)
+    }).returning("id");
 
-  // select all from devices table and log result
-  const allDevices = await db("devices").select();
-  console.log(allDevices);
+    console.log(insertedRows);
 
-  response.send("Device created!");
+    // Select all from devices table and log result
+    const allDevices = await db("devices").select();
+    console.log(allDevices);
+
+    response.send({
+      deviceId: insertedRows[0].id,
+    });
+  } catch (error) {
+    response.status(500).send("Error: " + error);
+  }
 });
