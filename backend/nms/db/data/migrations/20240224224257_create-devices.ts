8230ad4568f4e3304b0d446bee6028c1ff5be2ff
Jakobi Haskell <jakobi_haskell@brown.edu>
2024-03-21 00:26:57 -0400

    Feature/queue delete (#28)
    * feat: working on database for dashboard
    
    * adds support for knex locally in nms
    
    * adds stashed changes to knex for local dev
    
    * i think fixes knex in nms
    
    * updates readme
    
    * feat: table changes when interaction
    
    * adds working queue scheduler for notifications
    
    * in progress
    
    ---------
    
    Co-authored-by: Calvin Eng <calvineng72@gmail.com>

--- backend/nms/db/data/migrations/20240224224257_create-devices.ts
+++ backend/nms/db/data/migrations/20240224224257_create-devices.ts
@@ -14,19 +14,19 @@ export async function up(knex: Knex): Promise<void> {
         table.uuid("id").primary();
         table.string("email").unique().notNullable();
         table.string("name").notNullable();
-      })
-    )
-    .then(() =>
-      knex.schema.createTable("notifications", table => {
-        table.string("time").notNullable();
-        table.string("title").notNullable();
-        table.string("body").notNullable();
-      })
-    );
+      }))
+      .then(() =>
+        knex.schema.createTable("notifications", table => {
+          table.increments("id").primary();
+          table.string("time").notNullable();
+          table.string("title").notNullable();
+          table.string("body").notNullable();
+        })
+      );
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTableIfExists("users");
-  await knex.schema.dropTableIfExists("devices");
-  return knex.schema.dropTableIfExists("notifications");
+   await knex.schema.dropTableIfExists("users");
+   await knex.schema.dropTableIfExists("devices");
+   return knex.schema.dropTableIfExists("notifications");
 }
---
+++ backend/nms/pages/api/notifications/[jobId].ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import db from "../../../dist/data/db-config";
+
+type ResponseData = {
+  message: string;
+};
+
+type Notification = {
+  time: string;
+  title: string;
+  body: string;
+};
+
+export default async function getNotification(
+  req: NextApiRequest,
+  res: NextApiResponse<Notification[] | ResponseData>
+) {
+  try {
+    // get path param
+    const jobId = parseInt(req.query.jobId as string);
+
+    if (jobId) {
+      const notification = await db("notifications")
+        .select("*")
+        .where({ id: jobId });
+      res.status(200).json(notification[0]);
+    } else {
+      const notifications = await db("notifications").select("*");
+      res.status(200).json(notifications);
+    }
+  } catch (error) {
+    console.error("Error fetching notifications from the database:", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
+}
