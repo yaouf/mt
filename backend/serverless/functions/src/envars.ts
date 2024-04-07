e4fb9aeeaf38c6c5ce2d49e3cabdb0038cb9cc66
Jakobi Haskell <jakobi_haskell@brown.edu>
2024-04-06 22:56:24 -0400

    gets serverless/db working with staging

--- backend/serverless/functions/src/envars.ts
+++ backend/serverless/functions/src/envars.ts
@@ -1,6 +1,7 @@
 import { defineString } from "firebase-functions/params";
 
 const environment = defineString("ENV").value();
+console.log(`Environment: ${environment}`);
 const stagingDbUrl = defineString("DB_URL").value();
 const trustedApiKey = defineString("API_KEY").value();
 const envars = { environment, stagingDbUrl, trustedApiKey};
--- backend/serverless/functions/src/url/createDevice.ts
+++ backend/serverless/functions/src/url/createDevice.ts
@@ -4,7 +4,6 @@ import db from "../../../db/dist/data/db-config";
 import { v4 as uuidv4 } from "uuid";
 import envars from "../envars";
 export const createDevice = onRequest(async (request, response) => {
-  
   // Get the apiKey from the request headers
   const untrustedApiKey = request.get("X-API-KEY");
 
@@ -14,9 +13,9 @@ export const createDevice = onRequest(async (request, response) => {
   console.log(untrustedApiKey, trustedApiKey);
   // Check if the API key is correct
   if (!untrustedApiKey || untrustedApiKey !== trustedApiKey) {
-    response.status(401).send("Unauthorized")
+    response.status(401).send("Unauthorized");
     return;
-  } 
+  }
 
   logger.info("Creating a new device", { structuredData: true });
   // creates a new device in device table with deviceId, deviceType, breakingNewsAlerts, weeklySummaryAlerts, expoPushToken? (optional)
@@ -25,20 +24,30 @@ export const createDevice = onRequest(async (request, response) => {
   try {
     // Extract device information from the request body
     // The "|| {}" portion at the end ensures that if there are any issues with request.body line 12 won't throw an error
-    const {deviceType, breakingNewsAlerts, weeklySummaryAlerts, expoPushToken} = request.body || {};
-    if (!deviceType || typeof breakingNewsAlerts !== 'boolean' || typeof weeklySummaryAlerts !== 'boolean') {
+    const { deviceType, expoPushToken } = request.body || {};
+    const breakingNews = request.body["Breaking News"];
+    const weeklySummary = request.body["Weekly Summary"];
+    const dailySummary = request.body["Daily Summary"];
+    if (
+      !deviceType || !expoPushToken ||
+      typeof breakingNews !== "boolean" ||
+      typeof weeklySummary !== "boolean" ||
+      typeof dailySummary !== "boolean"
+    ) {
       response.status(400).send("Missing required fields in request body");
       return;
     }
-    // TODO: make this expoPushToken required
     // Insert the device into the devices table, and return the id of the inserted row
-    const insertedRows = await db(dbParams)("devices").insert({
-      id: uuidv4(), // Generate a new UUID for the device
-      deviceType: deviceType, 
-      breakingNewsAlerts: breakingNewsAlerts,
-      weeklySummaryAlerts: weeklySummaryAlerts,
-      expoPushToken: expoPushToken, // Should always exist, even if notifications were denied, but right now it's optional
-    }).returning("id");
+    const insertedRows = await db(dbParams)("devices")
+      .insert({
+        id: uuidv4(), // Generate a new UUID for the device
+        deviceType: deviceType,
+        "Breaking News": breakingNews,
+        "Weekly Summary": weeklySummary,
+        "Daily Summary": dailySummary,
+        expoPushToken: expoPushToken, // Should always exist, even if notifications were denied, but right now it's optional
+      })
+      .returning("id");
     // Only return id to the client if the insert was successful
     console.log(insertedRows);
 
@@ -53,5 +62,4 @@ export const createDevice = onRequest(async (request, response) => {
     console.error("Error creating device:", error);
     response.status(500).send("Error: " + error);
   }
-
 });
