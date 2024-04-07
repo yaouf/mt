ff6cbede4f8d4d3bb72d97a6aeaf63789d1f2792
Jakobi Haskell <jakobi_haskell@brown.edu>
2024-04-06 22:10:33 -0400

    updates migrations and seeds with nms db for firebase

--- backend/serverless/db/data/migrations/20240224224257_create-devices.ts
+++ backend/serverless/db/data/migrations/20240224224257_create-devices.ts
@@ -1,35 +1,39 @@
 import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-    await knex.schema
-      .createTable("devices", table => {
+  await knex.schema
+    .createTable("devices", table => {
+      table.uuid("id").primary();
+      table.string("deviceType").notNullable();
+      table.boolean("Breaking News").notNullable();
+      table.boolean("Weekly Summary").notNullable();
+      table.boolean("Daily Summary").notNullable();
+      table.string("expoPushToken").unique();
+    })
+    .then(() =>
+      knex.schema.createTable("users", table => {
         table.uuid("id").primary();
-        table.string("deviceType").notNullable();
+        table.string("email").unique().notNullable();
+        table.string("name").notNullable();
+      })
+    )
+    .then(() =>
+      knex.schema.createTable("notifications", table => {
+        table.increments("id").primary();
+        table.string("time").notNullable();
+        table.string("title").notNullable();
+        table.string("body").nullable();
+        table.string("slug").nullable();
         table.boolean("Breaking News").notNullable();
         table.boolean("Weekly Summary").notNullable();
         table.boolean("Daily Summary").notNullable();
-        table.string("expoPushToken").unique();
+        table.string("status").notNullable();
       })
-      .then(() =>
-        knex.schema.createTable("users", table => {
-          table.uuid("id").primary();
-          table.string("email").unique().notNullable();
-          table.string("name").notNullable();
-        })
-      )
-      .then(() =>
-        knex.schema.createTable("notifications", table => {
-          table.string("time").notNullable();
-          table.string("title").notNullable();
-          table.string("body").notNullable();
-        })
-      );
+    );
 }
-
 
 export async function down(knex: Knex): Promise<void> {
-   await knex.schema.dropTableIfExists("users");
-   await knex.schema.dropTableIfExists("devices");
-   return knex.schema.dropTableIfExists("notifications");
+  await knex.schema.dropTableIfExists("users");
+  await knex.schema.dropTableIfExists("devices");
+  return knex.schema.dropTableIfExists("notifications");
 }
-
