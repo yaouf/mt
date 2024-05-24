67fd90c37b1d39579b8aabf193fa85c502f90163
Jakobi Haskell <jakobi_haskell@brown.edu>
2024-05-24 16:13:28 -0400

    set up web server in playwright

--- backend/nms/playwright.config.ts
+++ backend/nms/playwright.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: '__integration_tests__',
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://127.0.0.1:3000',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
   use: {
     headless: true, // Run tests in headful mode
+    baseURL: 'http://localhost:3000/'
   },
 });
