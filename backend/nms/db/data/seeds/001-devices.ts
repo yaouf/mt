dd8a0b6bc6a8698120a121e7fac0cab5172ee81a
Jakobi Haskell <jakobi_haskell@brown.edu>
2024-09-05 15:01:32 -0400

    supports uuid notifications in nms (#86)
    * supports uuid notifs
    
    * changes name of pages handler, modifies all files to rwx

--- backend/nms/db/data/seeds/001-devices.ts
+++ backend/nms/db/data/seeds/001-devices.ts
@@ -1,40 +1,39 @@
 import { Knex } from "knex";
-import { v4 as uuidv4 } from "uuid";
 // TODO: rename this file to 001.ts
 export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries
   await knex("devices").del();
 
   // Inserts seed entries
-  await knex("devices").insert([
-    {
-      id: uuidv4(),
-      deviceType: "ios",
-      "Breaking News": true,
-      "University News": false,
-      "Metro": false,
-      expoPushToken: "ExponentPushToken[psEoIvGQmUy-WCOX2LVqq5]",
-      isPushEnabled: true,
-    },
-    {
-      id: uuidv4(),
-      deviceType: "ios",
-      "Breaking News": true,
-      "University News": false,
-      "Metro": false,
-      expoPushToken: "ExponentPushToken[mGGu2cGvMo9QjWr2QlsH9a]",
-      isPushEnabled: true,
-    },
-    {
-      id: uuidv4(),
-      deviceType: "ios",
-      "Breaking News": true,
-      "University News": false,
-      "Metro": false,
-      expoPushToken: "ExponentPushToken[IIDil0LpnR0mZj4irAdv_x]",
-      isPushEnabled: true,
-    },
-  ]);
+  // await knex("devices").insert([
+  //   {
+  //     id: uuidv4(),
+  //     deviceType: "ios",
+  //     "Breaking News": true,
+  //     "University News": false,
+  //     "Metro": false,
+  //     expoPushToken: "ExponentPushToken[psEoIvGQmUy-WCOX2LVqq5]",
+  //     isPushEnabled: true,
+  //   },
+  //   {
+  //     id: uuidv4(),
+  //     deviceType: "ios",
+  //     "Breaking News": true,
+  //     "University News": false,
+  //     "Metro": false,
+  //     expoPushToken: "ExponentPushToken[mGGu2cGvMo9QjWr2QlsH9a]",
+  //     isPushEnabled: true,
+  //   },
+  //   {
+  //     id: uuidv4(),
+  //     deviceType: "ios",
+  //     "Breaking News": true,
+  //     "University News": false,
+  //     "Metro": false,
+  //     expoPushToken: "ExponentPushToken[IIDil0LpnR0mZj4irAdv_x]",
+  //     isPushEnabled: true,
+  //   },
+  // ]);
 
   await knex("notifications").del();
 
