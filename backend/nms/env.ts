03c099cf30eb831b2a2f47f92948611a47f23063
Jakobi Haskell <jakobi_haskell@brown.edu>
2024-09-15 13:43:52 -0400

    wip: multiple env vars

--- backend/nms/env.ts
+++ backend/nms/env.ts
@@ -1,8 +1,22 @@
+import dotenv from "dotenv";
 import { cleanEnv, str } from "envalid";
+import path from "path";
+
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+
+const ENV = process.env.ENV ?? 'development';
+// const NODE_ENV = process.env.NODE_ENV ?? 'development';
+
+dotenv.config({ path: path.resolve(process.cwd(), `.env.${ENV}`) });
+console.log(path.resolve(process.cwd(), `.env.${ENV}`) );
+
+// dotenv.config({ path: path.resolve(process.cwd(), `.env.${NODE_ENV}`) });
+// console.log(path.resolve(process.cwd(), `.env.${NODE_ENV}`) );
 
 const env = cleanEnv(process.env, {
-    DB_URL: str({devDefault: ''}),
-  NODE_ENV: str({ choices: ['development', 'test', 'staging', 'production']  }),
+  DB_URL: str({devDefault: ''}),
+  NODE_ENV: str({ choices: ['development', 'test', 'production']  }),
+  ENV: str({choices: ['development', 'test', 'staging', 'production'], desc: 'This is used to specify app environment, since NODE_ENV only permits 3 values. '}),
   DB_USER: str({devDefault: ''}),
   DB_PASSWORD: str({devDefault: ''}),
   DB_NAME: str({devDefault: ''}),
