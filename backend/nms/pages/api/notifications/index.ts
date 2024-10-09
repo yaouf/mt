e47df4c536b3d80c40ab0668f29d16699af6d34c
Jakobi Haskell <jakobi_haskell@brown.edu>
2024-10-09 18:39:44 -0400

    wip: modify apis to work with denormalized db

--- backend/nms/pages/api/notifications/index.ts
+++ backend/nms/pages/api/notifications/index.ts
@@ -10,7 +10,18 @@ export default async function getNotifications(
   res: NextApiResponse<Notification[] | ResponseData>,
 ) {
   try {
-    const notifications = await db("notifications").select("id", "time", "title", "body", "status", "Breaking News", "University News", "Metro", "url", "isUid");
+//     SELECT n.id, n.title, STRING_AGG(nt.name, ', ') AS notification_types
+// FROM notifications n
+// JOIN subscriptions s ON s.notification_type_id = n.id -- Adjust this as needed
+// JOIN notification_types nt ON nt.id = s.notification_type_id
+// GROUP BY n.id, n.title;
+
+    const notifications = await db("notifications")
+      .select("id", "time", "title", "body", "status", "Breaking News", "University News", "Metro", "url", "isUid")
+      .join("subscriptions", "notifications.id", "subscriptions.notification_type_id")
+      .join("notification_types", "subscriptions.notification_type_id", "notification_types.id")
+      .groupBy("notifications.id", "notifications.title");
+    console.log("notifications", notifications);
     res.status(200).json(notifications);
   } catch (error) {
     console.error("Error fetching notifications from the database:", error);
