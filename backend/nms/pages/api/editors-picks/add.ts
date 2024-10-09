b3bef0fff33c2d19bc93de04a842aad2428b1825
Jakobi Haskell <jakobi_haskell@brown.edu>
2024-10-09 16:02:54 -0400

    replaces mock editors picks with actual db editors picsk

--- backend/nms/pages/api/editors-picks/add.ts
+++ backend/nms/pages/api/editors-picks/add.ts
@@ -1,10 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import db from "../../../dist/data/db-config";
 
-type ResponseData = {
-  message?: string;
-  jobId?: number;
-  picks?: EditorPick[];
-} | EditorPick | EditorPick[];
+type ResponseData =
+  | {
+      message?: string;
+      jobId?: number;
+      picks?: EditorPick[];
+    }
+  | EditorPick
+  | EditorPick[];
 
 interface EditorPick {
   id: number;
@@ -14,7 +18,7 @@ export default async function addEditorPick(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  
+  try {
     let data: any;
     if (typeof req.body === "string") {
       data = JSON.parse(req.body);
@@ -24,13 +28,26 @@ export default async function addEditorPick(
 
     const url = data.url;
 
+    console.log("url", url);
+    // Insert the notification data into the table
+    const insertedRows = await db("editorspicks")
+      .insert({
+        url: url,
+      })
+      .returning("id");
+    console.log("insertedRows", insertedRows);
 
     // Create a new editor's pick
     const newPick: EditorPick = {
       id: Date.now(), // Unique ID for the new pick
       url: url,
     };
-    
+
     // Respond with the new pick
     res.status(201).json(newPick);
-  } 
+    
+  } catch (error) {
+    console.error("Error deleting editors pick from the database:", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
+}
