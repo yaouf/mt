ad7f621714196cec500e832cfaea5bf326929287
Jakobi Haskell <jakobi_haskell@brown.edu>
2024-10-13 17:24:48 -0400

    adds haptic feedback to save button (#98)
    * adds haptic feedback strong to bottom nav bar
    
    * adds light haptic feedback to notif toggle and bottom article bar
    
    * removes haptic feedback from bottom nav bar
    
    * undoes bottom nav bar haptic feedback
    
    * removes haptic feedback from toggle since already has
    
    * formats bottom bar
    
    * adds todo

--- frontend/src/pages/article/BottomBar.tsx
+++ frontend/src/pages/article/BottomBar.tsx
@@ -1,41 +1,47 @@
-import { articleStyles } from "src/styles/article";
-import { View, ScrollView, Image, TouchableOpacity } from "react-native";
-import { shareArticle } from "./ShareArticle";
 import { Ionicons } from "@expo/vector-icons";
-import { useContext, useEffect, useState } from "react";
-import { SavedContext } from "../Nav";
-import { handleBookmark } from "src/code/helpers";
-import { ArticleDetailProps } from "src/types/other";
 import { useNavigation } from "@react-navigation/native";
+import * as Haptics from "expo-haptics";
+import { useContext, useEffect, useState } from "react";
+import { Image, TouchableOpacity, View } from "react-native";
+import { handleBookmark } from "src/code/helpers";
+import { articleStyles } from "src/styles/article";
+import { ArticleDetailProps } from "src/types/other";
+import { SavedContext } from "../Nav";
+import { shareArticle } from "./ShareArticle";
 
 /**
  * action bar at the bottom of each article
  * share, save, (notifications for this section / author in a future version)
  */
-
+// TODO: rename to BottomArticleBar
 function BottomBar(props: ArticleDetailProps) {
   const navigation = useNavigation();
   const { savedArticles, setSavedArticles } = useContext(SavedContext);
   const [saved, setSaved] = useState<boolean>(props.uuid in savedArticles);
-
+  // TODO: make the bottom bar buttons larger
   useEffect(() => {
     setSaved(props.uuid in savedArticles);
   }, [savedArticles, saved]);
 
   function handleShare() {
-    const split = props.published_at.split('-');
-    shareArticle(`https://www.browndailyherald.com/article/${split[0]}/${split[1]}/${props.slug}`);
+    const split = props.published_at.split("-");
+    shareArticle(
+      `https://www.browndailyherald.com/article/${split[0]}/${split[1]}/${props.slug}`
+    );
   }
 
   // not doing individual article/section notifs for now
   // function handleNotification() {}
 
   return (
-    <View style={articleStyles.actionBar} accessibilityLabel="Article Action Bar">
+    <View
+      style={articleStyles.actionBar}
+      accessibilityLabel="Article Action Bar"
+    >
       <View style={articleStyles.actions}>
         <TouchableOpacity
           onPress={() => navigation.goBack()}
-          style={{  }}
+          style={{}}
           accessibilityLabel="Back Button"
           accessibilityHint="Press to go back to the previous screen"
         >
@@ -49,10 +55,11 @@ function BottomBar(props: ArticleDetailProps) {
         </TouchableOpacity> */}
       </View>
 
-        <View style={articleStyles.actions}>
+      <View style={articleStyles.actions}>
         <TouchableOpacity
-          style={{paddingRight: 10}}
-          onPress={() =>
+          style={{ paddingRight: 10 }}
+          onPress={() => {
+            Haptics.selectionAsync(); // Added haptic feedback
             handleBookmark(
               saved,
               props.slug,
@@ -61,8 +68,8 @@ function BottomBar(props: ArticleDetailProps) {
               savedArticles,
               setSavedArticles,
               setSaved
-            )
-          }
+            );
+          }}
           accessible={true}
           accessibilityLabel="Bookmark Button"
           accessibilityHint="Press to bookmark or unbookmark the article"
@@ -73,10 +80,12 @@ function BottomBar(props: ArticleDetailProps) {
             <Ionicons name="bookmark-outline" size={24} color="#1C1B1F" />
           )}
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleShare()} 
-        accessible={true}
-        accessibilityLabel="Share Button"
-        accessibilityHint="Press to share the article">
+        <TouchableOpacity
+          onPress={() => handleShare()}
+          accessible={true}
+          accessibilityLabel="Share Button"
+          accessibilityHint="Press to share the article"
+        >
           <Image
             source={require("../../../assets/icons/share.png")}
             style={articleStyles.icon}
