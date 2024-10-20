c46d72e63c6d81c8a978e15b1ea968c28df94742
Jakobi Haskell <jakobi_haskell@brown.edu>
2024-10-19 21:55:00 -0400

    replaces env with node_env for nms/db and uses loading script to load specific .env.NODE_ENV files

--- backend/nms/db/data/db-config.ts
+++ backend/nms/db/data/db-config.ts
@@ -1,8 +1,8 @@
 import knex from 'knex';
 import config from '../knexfile';
 import env from './env';
-console.log("env in db-config", env.ENV);
-const db = knex(config[env.ENV]);
+console.log("env in db-config", env.NODE_ENV);
+const db = knex(config[env.NODE_ENV]);
 
 export default db;
 
