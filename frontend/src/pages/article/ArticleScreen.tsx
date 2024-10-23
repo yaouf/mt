84edbb431536c68400b0a0e2294c5503b6cca83e
Jakobi Haskell <jakobi_haskell@brown.edu>
2024-10-22 22:38:41 -0400

    Revert "Margins + row boxes in sectionpref (#107)" (#113)
    This reverts commit 9a1873ae0023a78788354db232373e2438d35852.

--- frontend/src/pages/article/ArticleScreen.tsx
+++ frontend/src/pages/article/ArticleScreen.tsx
@@ -138,10 +138,9 @@ function ArticleScreen({
                               .replaceAll("<p>", "")
                               .replaceAll("</p>", "")
                               .replaceAll("&nbsp;", " ")
-                              .replaceAll("<br>", "")
-                      : ""}
+                          : ""}
                         {article.dominantMedia.authors.length > 0 &&
-                          "Media by " +
+                          " Media by " +
                             article.dominantMedia.authors
                               .map((mediaAuthor) => mediaAuthor.name)
                               .join(", ") +
