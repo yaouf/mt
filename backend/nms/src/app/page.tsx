6e8d3b5f8118ebea35bc0e55c106d61c3f49395a
Calvin Eng <94716956+Calvineng72@users.noreply.github.com>
2024-10-27 20:50:51 -0700

    feat: add view specific to admin (#118)

--- backend/nms/src/app/page.tsx
+++ backend/nms/src/app/page.tsx
@@ -7,9 +7,11 @@ import EditorsPicks from "./EditorsPicks";
 import NotificationForm from "./NotificationForm";
 import NotificationTable from "./NotificationTable";
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
 export default function Home() {
-  const [scheduledNotifications, setScheduledNotifications] = useState<Notification[]>([]);    
+  const [scheduledNotifications, setScheduledNotifications] = useState<
+    Notification[]
+  >([]);
   const [editorsPicks, setEditorsPicks] = useState([] as any[]); // New state for editor's picks
   const [deviceCount, setDeviceCount] = useState<string>("Loading..."); // New state for device count
 
@@ -31,17 +33,16 @@ export default function Home() {
 
   useEffect(() => {
     const fetchDeviceCount = async () => {
-      try{
-        const response = await fetch("/api/devices/count");        
+      try {
+        const response = await fetch("/api/devices/count");
         const data = await response.json();
         setDeviceCount(data.count.toString());
-      }
-      catch(error){
+      } catch (error) {
         console.error("Error fetching device count:", error);
       }
     };
     fetchDeviceCount();
-  },[]);
+  }, []);
 
   useEffect(() => {
     const fetchEditorsPicks = async () => {
@@ -54,51 +55,67 @@ export default function Home() {
 
   return (
     <AuthWrapper>
-      {/* Display environment variables */}
-      <div className="flex justify-center py-3">
-        <div className="flex flex-col items-start space-y-2 px-5 md:px-20 py-3 border border-gray-300 rounded-md bg-gray-50">
-          <h2 className="text-lg font-semibold text-black mb-2">Environment Variables</h2>
-          <p className="text-gray-700 flex">
-            <span className="font-bold mr-2">NODE_ENV</span> 
-            <span className={`${isProduction ? 'text-red-500' : 'text-gray-700'}`}>
-              {process.env.NODE_ENV}
-            </span>
-          </p>
-          <p className="text-gray-700 flex">
-            <span className="font-bold mr-2">NEXT_PUBLIC_ENV</span> 
-            <span className={`${isProduction ? 'text-red-500' : 'text-gray-700'}`}>
-              {process.env.NEXT_PUBLIC_ENV}
-            </span>
-          </p>
-        </div>
-      </div>
+      {(user) => (
+        <>
+          {/* Display environment variables */}
+          <div className="flex justify-center py-3">
+            <div className="flex flex-col items-start space-y-2 px-5 md:px-20 py-3 border border-gray-300 rounded-md bg-gray-50">
+              <h2 className="text-lg font-semibold text-black mb-2">
+                Environment Variables
+              </h2>
+              <p className="text-gray-700 flex">
+                <span className="font-bold mr-2">NODE_ENV</span>
+                <span
+                  className={`${
+                    isProduction ? "text-red-500" : "text-gray-700"
+                  }`}
+                >
+                  {process.env.NODE_ENV}
+                </span>
+              </p>
+              <p className="text-gray-700 flex">
+                <span className="font-bold mr-2">NEXT_PUBLIC_ENV</span>
+                <span
+                  className={`${
+                    isProduction ? "text-red-500" : "text-gray-700"
+                  }`}
+                >
+                  {process.env.NEXT_PUBLIC_ENV}
+                </span>
+              </p>
+            </div>
+          </div>
 
-      <div className="flex justify-center py-3">
-        <div className="flex flex-col items-start space-y-2 px-5 md:px-20 py-3 border border-gray-300 rounded-md bg-gray-50">
-          <p className="text-gray-700 flex">
-            <span className="font-bold mr-2">Total devices</span> 
-            <span className="text-gray-700">
-              {deviceCount}
-            </span>
-          </p>
-        </div>
-      </div>
-      
-      <main className="flex min-h-screen flex-col items-center justify-between md:px-20 py-3">
-        <NotificationTable
-          scheduledNotifications={scheduledNotifications}
-          setScheduledNotifications={setScheduledNotifications}
-        />
-              
+          <div className="flex justify-center py-3">
+            <div className="flex flex-col items-start space-y-2 px-5 md:px-20 py-3 border border-gray-300 rounded-md bg-gray-50">
+              <p className="text-gray-700 flex">
+                <span className="font-bold mr-2">Total devices</span>
+                <span className="text-gray-700">{deviceCount}</span>
+              </p>
+            </div>
+          </div>
 
-        <NotificationForm setScheduledNotifications={setScheduledNotifications} />
-        <div className="flex justify-center py-10"></div>
-          <EditorsPicks
-          editorsPicks={editorsPicks} // Pass the editorsPicks state
-          setEditorsPicks={setEditorsPicks} // Pass the state setter function
-        />
-          
-      </main>
+          <main className="flex min-h-screen flex-col items-center justify-between md:px-20 py-3">
+            <NotificationTable
+              scheduledNotifications={scheduledNotifications}
+              setScheduledNotifications={setScheduledNotifications}
+            />
+
+            {user.email === "admin@example.com" && (
+              <>
+                <NotificationForm
+                  setScheduledNotifications={setScheduledNotifications}
+                />
+                <div className="flex justify-center py-10"></div>
+                <EditorsPicks
+                  editorsPicks={editorsPicks} // Pass the editorsPicks state
+                  setEditorsPicks={setEditorsPicks} // Pass the state setter function
+                />
+              </>
+            )}
+          </main>
+        </>
+      )}
     </AuthWrapper>
   );
 }
