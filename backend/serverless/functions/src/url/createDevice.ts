0455e16f3b8b96612a674a9f5df0b21fe6898612
Jakobi Haskell <jakobi_haskell@brown.edu>
2024-11-17 17:51:52 -0500

    updates backend apis with new notif sections, makes them all optional

--- backend/serverless/functions/src/url/createDevice.ts
+++ backend/serverless/functions/src/url/createDevice.ts
@@ -8,14 +8,18 @@ import { validateApiKey } from "../utils";
 
 export const createDevice = onRequest(async (request, response) => {
   if (!validateApiKey(request, response)) return;
-  
+
   const environment = envars.environment.value();
   const dbUrl = envars.dbUrl.value();
   const dbParams = { environment, dbUrl };
   const newDb = db(dbParams);
   logger.info("dbParams: ", dbParams);
 
-  logger.info("createDevice was called with the following request body: ", { structuredData: true }, request.body);
+  logger.info(
+    "createDevice was called with the following request body: ",
+    { structuredData: true },
+    request.body
+  );
   // creates a new device in device table with deviceId, deviceType, breakingNewsAlerts, universityNewsAlerts, expoPushToken? (optional)
   // Assume info above is in request body as json. If any required fields are missing, return an error status code
 
@@ -27,24 +31,27 @@ export const createDevice = onRequest(async (request, response) => {
     const schema = Joi.object({
       deviceType: Joi.string().required(),
       expoPushToken: Joi.string().required(),
-      "Breaking News": Joi.boolean().required(),
-      "University News": Joi.boolean().required(),
-      "Metro": Joi.boolean().required(),
-      "Opinions": Joi.boolean().required(),
-      "Arts and Culture": Joi.boolean().required(),
-      "Sports": Joi.boolean().required(),
-      "Science and Research": Joi.boolean().required(),
-      isPushEnabled: Joi.boolean().required(),
+      "Breaking News": Joi.boolean().optional(),
+      "University News": Joi.boolean().optional(),
+      Metro: Joi.boolean().optional(),
+      Opinions: Joi.boolean().optional(),
+      "Arts and Culture": Joi.boolean().optional(),
+      Sports: Joi.boolean().optional(),
+      "Science and Research": Joi.boolean().optional(),
+      isPushEnabled: Joi.boolean().optional(),
     });
 
     // Validate request body
     const { error, value: validBody } = schema.validate(request.body);
     if (error) {
-      logger.error("Request body validation error: " + error.message, { requestBody: request.body });
-      response.status(400).send("Request body validation error: " + error.message);
+      logger.error("Request body validation error: " + error.message, {
+        requestBody: request.body,
+      });
+      response
+        .status(400)
+        .send("Request body validation error: " + error.message);
       return;
     }
-
 
     const deviceType = validBody["deviceType"];
     const expoPushToken = validBody["expoPushToken"];
@@ -62,78 +69,97 @@ export const createDevice = onRequest(async (request, response) => {
     const existingDevice = await newDb("devices")
       .where("expoPushToken", expoPushToken)
       .first();
-      // Initialize deviceId
-      let deviceId: string; 
+    // Initialize deviceId
+    let deviceId: string;
     if (existingDevice) {
+      const updateFields: Record<string, any> = {
+        deviceType,
+        "Breaking News": breakingNews,
+        "University News": universityNews,
+        Metro: metro,
+        Opinions: opinions,
+        "Arts and Culture": artsAndCulture,
+        Sports: sports,
+        "Science and Research": scienceAndResearch,
+        isPushEnabled,
+      };
+
+      const filteredUpdateFields = Object.fromEntries(
+        Object.entries(updateFields).filter(
+          ([_, value]) => value !== undefined && value !== null
+        )
+      );
+
       // Update the device's settings
       await newDb("devices")
         .where("expoPushToken", expoPushToken)
-        .update({
-          deviceType: deviceType,
-          "Breaking News": breakingNews,
-          "University News": universityNews,
-          "Metro": metro,
-          "Opinions": opinions,
-          "Arts and Culture": artsAndCulture,
-          "Sports": sports,
-          "Science and Research": scienceAndResearch,
-          isPushEnabled: isPushEnabled,
-        });
-        deviceId = existingDevice.id;
-        // TODO: refactor this into a single object
-        logger.info("CreateDevice was called but device already exists. Device updated for deviceId: ", { deviceId, updates: {
-          deviceType: deviceType,
-          "Breaking News": breakingNews,
-          "University News": universityNews,
-          "Metro": metro,
-          "Opinions": opinions,
-          "Arts and Culture": artsAndCulture,
-          "Sports": sports,
-          "Science and Research": scienceAndResearch,
-          isPushEnabled: isPushEnabled,
-        } });
+        .update(filteredUpdateFields);
+      deviceId = existingDevice.id;
+      // TODO: refactor this into a single object
+      logger.info(
+        "CreateDevice was called but device already exists. Device updated for deviceId: ",
+        {
+          deviceId,
+          updates: {
+            deviceType: deviceType,
+            "Breaking News": breakingNews,
+            "University News": universityNews,
+            Metro: metro,
+            Opinions: opinions,
+            "Arts and Culture": artsAndCulture,
+            Sports: sports,
+            "Science and Research": scienceAndResearch,
+            isPushEnabled: isPushEnabled,
+          },
+        }
+      );
     } else {
       // Insert the device into the devices table, and return the id of the inserted row
-    const insertedRows = await newDb("devices")
-    .insert({
-      id: uuidv4(), // Generate a new UUID for the device
-      deviceType: deviceType,
-      "Breaking News": breakingNews,
-      "University News": universityNews,
-      "Metro": metro,
-      "Opinions": opinions,
-      "Arts and Culture": artsAndCulture,
-      "Sports": sports,
-      "Science and Research": scienceAndResearch,
-      isPushEnabled: isPushEnabled,
-      expoPushToken: expoPushToken, // Should always exist, even if notifications were denied, but right now it's optional
-    })
-    .returning("id");
+      const insertedRows = await newDb("devices")
+        .insert({
+          id: uuidv4(), // Generate a new UUID for the device
+          deviceType: deviceType,
+          "Breaking News": breakingNews ?? false,
+          "University News": universityNews ?? false,
+          Metro: metro ?? false,
+          Opinions: opinions ?? false,
+          "Arts and Culture": artsAndCulture ?? false,
+          Sports: sports ?? false,
+          "Science and Research": scienceAndResearch ?? false,
+          isPushEnabled: isPushEnabled ?? false,
+          expoPushToken: expoPushToken, // Should always exist, even if notifications were denied, but right now it's optional
+        })
+        .returning("id");
 
-    await newDb.destroy();
-    // TODO: change expo push token to required field
-    // logger.info("inserted row: ", insertedRows);
-    deviceId = insertedRows[0].id;
-    // TODO: structure logs so I can filter by function name in logger
-    logger.info("Device created. Inserted deviceId: ", deviceId, " with the following settings: ", {
-      deviceType: deviceType,
-      "Breaking News": breakingNews,
-      "University News": universityNews,
-      "Metro": metro,
-      "Opinions": opinions,
-      "Arts and Culture": artsAndCulture,
-      "Sports": sports,
-      "Science and Research": scienceAndResearch,
-      isPushEnabled: isPushEnabled,
-    });
+      await newDb.destroy();
+      // TODO: change expo push token to required field
+      // logger.info("inserted row: ", insertedRows);
+      deviceId = insertedRows[0].id;
+      // TODO: structure logs so I can filter by function name in logger
+      logger.info(
+        "Device created. Inserted deviceId: ",
+        deviceId,
+        " with the following settings: ",
+        {
+          deviceType: deviceType,
+          "Breaking News": breakingNews,
+          "University News": universityNews,
+          Metro: metro,
+          Opinions: opinions,
+          "Arts and Culture": artsAndCulture,
+          Sports: sports,
+          "Science and Research": scienceAndResearch,
+          isPushEnabled: isPushEnabled,
+        }
+      );
     }
-     // Select all from devices table and log result
+    // Select all from devices table and log result
     //  const allDevices = await db(dbParams)("devices").select();
     // logger.info(allDevices);
-     // Send the device ID back to the client
-   response.send({
-     deviceId: deviceId,
-   });
+    // Send the device ID back to the client
+    response.send({
+      deviceId: deviceId,
+    });
   } catch (error) {
     console.error("Error creating device:", error);
     response.status(500).send("Error: " + error);
