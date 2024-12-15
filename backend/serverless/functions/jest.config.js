4ad97907c3205774e251d84919e067ce47c530c7
Sana Saab <sana_saab@brown.edu>
2024-12-14 23:03:12 -0500

    Backend/sana/feature/serverless testing (#124)
    * replaces cd with --prefix for windows compatibility
    
    * adds cross-env package for windows compat and uses it everywhere I set env variables in scripts (NOT yet load-env.js, not sure nec)
    
    * Replaces & with concurrently for windows support
    
    * replaces unix script with cross-port-killer for kill:redis script
    
    * feat: test file set up
    
    * serveless firebase functions test pass.
    please make sure to run migration first by: npm run knex -- migrate:latest
    
    also in future try to keep db folders in sync symbolically, and also look into test hanging
    
    ---------
    
    Co-authored-by: Jakobi Haskell <jakobi_haskell@brown.edu>

---
+++ backend/serverless/functions/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: {
+        strict: false,
+        noImplicitAny: false,
+        strictNullChecks: false,
+        strictFunctionTypes: false,
+        strictBindCallApply: false,
+        strictPropertyInitialization: false,
+        noImplicitThis: false,
+        alwaysStrict: false,
+      }
+    }],
+  },
+}; 
---
+++ backend/serverless/functions/test/index.test.ts
@@ -0,0 +1,46 @@
+import { describe, expect, it } from "@jest/globals";
+import { createDevice } from "../src/url/createDevice";
+
+jest.mock("../src/utils", () => ({
+  validateApiKey: jest.fn().mockResolvedValue(true),
+}));
+
+describe("createDevice", () => {
+  it("should create a new device successfully", async () => {
+    const req = {
+      body: {
+        deviceType: "iOS",
+        expoPushToken: "ExponentPushToken[unique-token-123]",
+        "Breaking News": true,
+        "University News": true,
+        Metro: true,
+        Opinions: true,
+        "Arts and Culture": true,
+        Sports: true,
+        "Science and Research": true,
+        isPushEnabled: true,
+      },
+      headers: {
+        "X-API-KEY": "test-api-key",
+      },
+      rawBody: Buffer.from(""),
+      get(header) {
+        return this.headers[header];
+      },
+    } as unknown as Request;
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    };
+
+    await createDevice(req as any, res as any);
+
+    // Check if response was called with a deviceId
+    expect(res.send).toHaveBeenCalledWith(
+      expect.objectContaining({
+        deviceId: expect.any(String),
+      })
+    );
+  });
+});
