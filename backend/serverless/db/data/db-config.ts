c6a83872c04912f0535cbe255bb0ae5479a5907f
Jakobi Haskell <jakobi_haskell@brown.edu>
2025-02-04 09:04:54 -0500

    updated data in servereless to be in sync with nms

--- backend/serverless/db/data/db-config.ts
+++ backend/serverless/db/data/db-config.ts
@@ -1,9 +1,8 @@
 import knex from 'knex';
 import config from '../knexfile';
-function outerDb(params: Record<string, string>) {
-    const { environment, dbUrl } = params;
-    const db = knex(config(dbUrl)[environment || "development"]);
-    return db;
-}
-export default outerDb;
+import env from './env';
+console.log("env in db-config", env.ENV);
+const db = knex(config[env.ENV]);
+
+export default db;
 
---
+++ backend/serverless/db/data/migrations/20241206215535_editors_picks_rank-required.ts
@@ -0,0 +1,18 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  console.log("Database connection config:", {
+    database: process.env.DB_URL,
+  });
+  return knex.schema.alterTable("editorspicks", (table) => {
+    // Remove default value and add unique constraint
+    table.integer("rank").notNullable().alter();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.alterTable("editorspicks", (table) => {
+    // Restore default value and remove unique constraint
+    table.integer("rank").notNullable().defaultTo(999).alter();
+  });
+}
