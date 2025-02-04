32e8abc4d2f713e0c482a0483a392501c80f4323
Jakobi Haskell <jakobi_haskell@brown.edu>
2025-02-04 09:50:49 -0500

    adds envalid to serverless functions.
    - does not add envalid as dep for functions,  cant remember if its necessary to keep deps in sync. but if there is then will make note of that.

---
+++ backend/serverless/db/data/env.ts
@@ -0,0 +1,21 @@
+import { cleanEnv, str } from 'envalid';
+
+import dotenv from 'dotenv';
+
+// // Load environment variables from .env file
+dotenv.config();
+
+const env = cleanEnv(process.env, {
+  DB_URL: str({devDefault: ''}),
+  NODE_ENV: str({ choices: ['development', 'test', 'production', 'staging'] }),
+  ENV: str({ choices: ['development', 'test', 'production', 'staging'] }),
+  DB_USER: str({devDefault: ''}),
+  DB_PASSWORD: str({devDefault: ''}),
+  DB_NAME: str({devDefault: ''}),
+})
+
+if (['staging'].includes(env.NODE_ENV) && !env.DB_URL) {
+  throw new Error('DB_URL is required in staging environment');
+}
+
+export default env;
