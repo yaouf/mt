518b4010e865125ddf881ff1ed5ab73f47201729
Jakobi Haskell <jakobi_haskell@brown.edu>
2025-02-04 09:49:14 -0500

    adds db config updated v

--- backend/serverless/db/data/db-config.ts
+++ backend/serverless/db/data/db-config.ts
@@ -1,9 +1,7 @@
-import knex from 'knex';
-import config from '../knexfile';
-function outerDb(params: Record<string, string>) {
-    const { environment, dbUrl } = params;
-    const db = knex(config(dbUrl)[environment || "development"]);
-    return db;
-}
-export default outerDb;
+import knex from "knex";
+import config from "../knexfile";
+import env from "./env";
+console.log("env in db-config", env.ENV);
+const db = knex(config[env.ENV as keyof typeof config]);
 
+export default db;
