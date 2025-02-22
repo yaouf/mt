62eeb23f942e65ff8073718fa79aa88857bb90ee
Calvin Eng <calvineng72@gmail.com>
2025-02-22 16:24:15 -0500

    docs: add comment to explain unique removal

--- backend/nms/db/data/migrations/20250213034702_remove_unique.ts
+++ backend/nms/db/data/migrations/20250213034702_remove_unique.ts
@@ -2,6 +2,7 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.alterTable("editors_picks", (table) => {
+    // Remove "editorspicks_rank_unique" since the table used to be called "editorspicks"
     table.dropUnique(["rank"], "editorspicks_rank_unique");
   });
 }
