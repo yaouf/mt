747f01c0d1f69531d3aafe5ec06650d9b8e40c36
Sophia Li <144983488+sophiaaali@users.noreply.github.com>
2025-02-22 16:58:29 -0500

    fix: notif enabled device counter works (#158)
    * fix: notif enabled device counter works
    
    * integrates with consolidation
    
    ---------
    
    Co-authored-by: Jakobi Haskell <jakobi_haskell@brown.edu>

--- backend/nms/pages/api/devices/count.ts
+++ backend/nms/pages/api/devices/count.ts
@@ -1,8 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import db from "../../../dist/data/db-config";
 import corsMiddleware from "../../../config/cors";
+import db from "../../../dist/data/db-config";
 import { authMiddleware } from "../../../middleware/authMiddleware";
-
 
 type ResponseData = {
   count?: string;
@@ -10,16 +9,34 @@ type ResponseData = {
 };
 
 async function getDeviceCountHelper(
-
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
   try {
     const search = req.query.search ? (req.query.search as string) : "";
     let result;
-    if (search!="") {
-      result = await db("devices").count("* as count").where(search,true);
+
+    if (search !== "") {
+      if (search === "isPushEnabled") {
+        // Use the custom logic for push enabled count
+        result = await db("devices")
+          .count("* as count")
+          .where((builder) => {
+            builder
+              .where("Breaking News", true)
+              .orWhere("University News", true)
+              .orWhere("Metro", true)
+              .orWhere("Sports", true)
+              .orWhere("Arts and Culture", true)
+              .orWhere("Science and Research", true)
+              .orWhere("Opinions", true);
+          });
+      } else {
+        // For other search queries, use the standard where clause
+        result = await db("devices").count("* as count").where(search, true);
+      }
     } else {
+      // When no search query is provided, count all devices
       result = await db("devices").count("* as count");
     }
 
@@ -27,7 +44,7 @@ async function getDeviceCountHelper(
     console.log("count", count);
 
     res.status(200).json({ count });
-  } catch (error) {
+  } catch (error: any) {
     console.error("Error fetching device count from the database:", error);
     res.status(500).json({ message: error.message });
   }
