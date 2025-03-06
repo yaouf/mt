3cdd66455d71a08994aa1faab9cc85358875257d
Jakobi Haskell <jakobi_haskell@brown.edu>
2025-03-06 14:22:22 -0500

    update again

--- backend/nms/db/data/migrations/20250213034702_remove_unique.ts
+++ backend/nms/db/data/migrations/20250213034702_remove_unique.ts
@@ -1,25 +1,17 @@
 import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-  // First identify if the constraint exists, then drop it
+  // Simpler approach to drop all unique constraints on the editors_picks table
   return knex.raw(`
-    IF EXISTS (
-      SELECT * FROM sys.key_constraints 
-      WHERE parent_object_id = OBJECT_ID('editors_picks') 
-      AND type = 'UQ'
-    )
-    BEGIN
-      DECLARE @constraintName nvarchar(128)
-      SELECT @constraintName = name 
-      FROM sys.key_constraints 
-      WHERE parent_object_id = OBJECT_ID('editors_picks') 
-      AND type = 'UQ'
-      AND COL_NAME(parent_object_id, parent_column_id) = 'rank'
-      
-      DECLARE @sql nvarchar(500)
-      SET @sql = 'ALTER TABLE editors_picks DROP CONSTRAINT ' + @constraintName
-      EXEC sp_executesql @sql
-    END
+    DECLARE @sql NVARCHAR(MAX) = '';
+    
+    SELECT @sql = @sql + 'ALTER TABLE editors_picks DROP CONSTRAINT ' + name + ';'
+    FROM sys.key_constraints
+    WHERE parent_object_id = OBJECT_ID('editors_picks')
+    AND type = 'UQ';
+    
+    IF LEN(@sql) > 0
+      EXEC sp_executesql @sql;
   `);
 }
 
