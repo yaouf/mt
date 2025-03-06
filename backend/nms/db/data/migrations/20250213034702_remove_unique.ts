a7030a80ec29a39e0baf16664390bf17daf5d000
Jakobi Haskell <jakobi_haskell@brown.edu>
2025-03-06 14:17:16 -0500

    update remove unique

--- backend/nms/db/data/migrations/20250213034702_remove_unique.ts
+++ backend/nms/db/data/migrations/20250213034702_remove_unique.ts
@@ -3,12 +3,12 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.alterTable("editors_picks", (table) => {
     // Remove "editorspicks_rank_unique" since the table used to be called "editorspicks"
-    table.dropUnique(["rank"], "editorspicks_rank_unique");
+    table.dropUnique(["rank"]);
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
   return knex.schema.alterTable("editors_picks", (table) => {
-    table.unique(["rank"], "editorspicks_rank_unique");
+    table.unique(["rank"]);
   });
 }
