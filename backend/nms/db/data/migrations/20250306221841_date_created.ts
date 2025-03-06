1832864d475a5df17f00118f07c96e2a27b464dc
Jakobi Haskell <jakobi_haskell@brown.edu>
2025-03-06 17:21:29 -0500

    updates syntax

--- backend/nms/db/data/migrations/20250306221841_date_created.ts
+++ backend/nms/db/data/migrations/20250306221841_date_created.ts
@@ -1,13 +1,10 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable("devices", (table) => {
     table.renameColumn("dateCreated", "date_created");
   });
 }
-}
-
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.alterTable("devices", (table) => {
